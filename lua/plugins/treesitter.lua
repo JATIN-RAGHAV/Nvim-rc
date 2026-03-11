da1f750766feec91e0981c4d3c740dceb9c4d64b
Walrus <jrag137@gmail.com>
2026-03-11 09:13:06 +0530

    removed which-key added autotags treesitter still broken

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,11 +1,12 @@
 return {
     "nvim-treesitter/nvim-treesitter",
-    -- config = function()
-    --     require('nvim-treesitter.configs').setup {
-    --         ensure_installed = { "c", "cpp", "lua", "python", "ruby", "zig", "swift", "javascript", "typescript"},
-    --         highlight = {
-    --             enable = true,
-    --         },
-    --     }
-    -- end
+    config = function()
+        require('nvim-treesitter').install{
+            "c", "cpp", "lua", "python", "javascript", "typescript", "tsx", "jsx","markdown"
+        }
+        vim.api.nvim_create_autocmd('FileType', {
+            pattern = { '<filetype>' },
+            callback = function() print("hi");vim.treesitter.start() end,
+        })
+    end
 }
