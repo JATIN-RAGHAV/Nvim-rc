b87b8045d37f6af29b5b9132d311c0ffa4b39621
Walrus <jrag137@gmail.com>
2026-03-14 20:05:25 +0530

    treesitter works frfr

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -5,14 +5,16 @@ return {
         require'nvim-treesitter'.setup {
             install_dir = vim.fn.stdpath('data') .. '/site'
         }
-        require'nvim-treesitter'.install {'lua', 'rust', 'javascript', 'typescript' ,'tsx','cpp','c'}:wait(30000)
+        require'nvim-treesitter'.install {'lua', 'rust', 'javascript', 'typescript','jsx' ,'tsx','cpp','c'}:wait(30000)
         vim.g.indentexpr = "v:lua.require'nvim-treesitter'.indentexpr()"
         vim.wo[0][0].foldexpr = 'v:lua.vim.treesitter.foldexpr()'
         vim.wo[0][0].foldmethod = 'expr'
 
-        vim.api.nvim_create_autocmd('FileType', {
-            pattern = { 'lua','rust','typescript','javascript' ,'cpp'},
-            callback = function() vim.treesitter.start() end,
+        vim.api.nvim_create_autocmd({"BufReadPost", "BufNewFile"}, {
+            pattern = {"*.tsx","*.jsx","*.ts","*.js","*.cpp","*.c","*.h","*.lua","*.py"},
+            callback = function()
+                vim.treesitter.start()
+            end,
         })
 
     end
